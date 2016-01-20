76d38c45d5a44b2ed2bbc28a2044c90d5b43872c
trueneu <true.neu@gmail.com>
2016-01-20 21:10:14 +0300

    updated sk-ssh-module help message

--- sk-modules/sk-ssh-module.py
+++ sk-modules/sk-ssh-module.py
@@ -214,9 +214,11 @@ class SSHPlugin(sk_classes.SKCommandPlugin):
                  'pssh': {'requires_hostlist': True},
                  'dist': {'requires_hostlist': True},
                  'gather': {'requires_hostlist': True}}
-    _commands_help_message = "ssh - execute a command over ssh host by host.\npssh - parallel exec.\ndist - distribute " \
+    _commands_help_message = "ssh - execute a command over ssh host by host (cmd)\npssh - parallel exec (cmd)\n" \
+                             "dist - distribute " \
                              "a file over ssh to hosts (source [destination, default is cwd])\ngather - gather " \
-                             "remote files from hosts to local machine (source [destination, default is cwd]\n"
+                             "remote files from hosts to local machine (source [destination, default is cwd]). " \
+                             "gather adds a suffix (_hostname) to local filenames\n"
 
     def __init__(self, *args, **kwargs):
         super(SSHPlugin, self).__init__(*args, **kwargs)
